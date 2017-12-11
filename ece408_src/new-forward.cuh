d388e9d89aa883a6edc664954b6b943504a48856
Chenhao Wu <chenhaowu@ChenhaotekiMacBook-Pro.local>
2017-12-11 00:26:56 -0600

    Add buggy Shared memory implementation

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -24,7 +24,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    const int W_grid = ceil(W_out*1.0/TILE_WIDTH);
+    const int W_grid = ceil(W_out*1.0/TILE_WIDTH); // width of gird
    // (void)H_out; // silence declared but never referenced warning. remove this line when you start working
    // (void)W_out; // silence declared but never referenced warning. remove this line when you start working
     // An example use of these macros:
@@ -39,22 +39,45 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     */
     int n = blockIdx.x;
     int m = blockIdx.y;
-    int h = blockIdx.z/W_grid * TILE_WIDTH + threadIdx.y;
-    int w = blockIdx.z%W_grid * TILE_WIDTH + threadIdx.x;
+    int h0 = threadIdx.x;
+    int w0 = threadIdx.y;
 
-    if(h < H_out && w < W_out){
-        float acc = 0;
-        for(int c = 0; c < C; c++){
+    int h_base = blockIdx.z/W_grid * TILE_WIDTH; // elementwize starting height index
+    int w_base = blockIdx.z%W_grid * TILE_WIDTH; // elementwise starting width index
+    int h = h_base + threadIdx.y; // actual height index 
+    int w = w_base + threadIdx.x; //  actual width index
+    
+    int x_tile_width = TILE_WIDTH + K - 1; // input tile size
+    //declare shared memory
+    extern __shared__ float shmem[];
+    float* X_shared = &shmem[0];
+    float* W_shared = &shmem[x_tile_width*x_tile_width]; // starting address of shared weights
+
+    float acc = 0.0;
+    for(int c = 0; c < C; c++){ // number of feature maps
+        if(h < H_out && w < W_out){
+            if(h0 < K && w0 < K){
+                W_shared[w0*K+h0] = k4d(m,c, w0, h0);
+            }
+            __syncthreads();
+            for(int i = h;i < h_base + x_tile_width; i += TILE_WIDTH){
+                for(int j = w; j < w_base + x_tile_width; j += TILE_WIDTH)
+
+                    X_shared[(i-h_base)*x_tile_width + j-w_base] = x4d(n,c,i,j);
+            }
+            __syncthreads();
+
             for(int p = 0; p < K; p++){
                 for(int q = 0; q < K; q++)
                 {
-                    acc += x4d(n,c,h+p,w+q) * k4d(m,c,p,q);
+                    acc += X_shared[(h0 + p)*x_tile_width + w0 + q] * W_shared[p*K + q];
                 }
+                
             }
         }
-        y4d(n,m,h,w) = acc; 
-    }
-    
+        __syncthreads();
+    }    
+    y4d(n,m,h,w) = acc; 
 
     #undef y4d
     #undef x4d
@@ -82,23 +105,23 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
-    const int B = x.shape_[0];
-    const int M = y.shape_[1];
-    const int C = x.shape_[1];
-    const int H = x.shape_[2];
-    const int W = x.shape_[3];
-    const int K = w.shape_[3];
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
+    const int B = x.shape_[0]; // number of inputs
+    const int M = y.shape_[1]; // number of output feature maps
+    const int C = x.shape_[1]; // number of input feature maps
+    const int H = x.shape_[2]; // height of each input feature map
+    const int W = x.shape_[3]; // width of each input feature map
+    const int K = w.shape_[3]; // width of filter
+    const int H_out = H - K + 1; // height of each output feature map
+    const int W_out = W - K + 1; // width of each output feature map
     const int W_grid = ceil(W_out*1.0/TILE_WIDTH);
     const int H_grid = ceil(H_out*1.0/TILE_WIDTH);
     const int Z = H_grid*W_grid;
     // Set the kernel dimensions
     dim3 gridDim(B,M,Z);
     dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
-
+    size_t shmem_size = sizeof(float*)*((TILE_WIDTH + K - 1)*(TILE_WIDTH + K - 1) + K*K);
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim, shmem_size, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
